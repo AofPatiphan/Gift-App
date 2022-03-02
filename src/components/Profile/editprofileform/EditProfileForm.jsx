0987efa130bc962a10457f949398025822fdb2fa
NewChakrit <imnew108@gmail.com>
2022-03-02 02:17:12 -0800

    'configUI'

--- src/components/Profile/editprofileform/EditProfileForm.jsx
+++ src/components/Profile/editprofileform/EditProfileForm.jsx
@@ -1,132 +1,122 @@
-import React, { useContext, useState } from 'react';
-import './editprofileform.css';
-import Select from 'react-select';
-import { interestOption } from '../../auths/register/docs/data';
-import { UserContext } from '../../../contexts/UserContext';
-import axios from '../../../config/axios';
+import React, { useContext, useState } from "react";
+import "./editprofileform.css";
+import Select from "react-select";
+import { interestOption } from "../../auths/register/docs/data";
+import { UserContext } from "../../../contexts/UserContext";
+import axios from "../../../config/axios";
 
 function EditProfileForm({ person, setPerson, username }) {
-    const { userData } = useContext(UserContext);
-    const [date, setDate] = useState(person.About.birthDate || '');
-    const [age, setAge] = useState(person.About.age || '');
-    const [profileCaption, setProfileCaption] = useState(
-        person.About.caption || ''
-    );
-    const [editInterest, setEditInterest] = useState(
-        person.About.interest || ''
-    );
+  const { userData } = useContext(UserContext);
+  const [date, setDate] = useState(person.About.birthDate || "");
+  const [age, setAge] = useState(person.About.age || "");
+  const [profileCaption, setProfileCaption] = useState(
+    person.About.caption || ""
+  );
+  const [editInterest, setEditInterest] = useState(person.About.interest || "");
 
-    const handleChangeToggle = (value) => {
-        const result = [];
-        value.map((item) => result.push(item.value));
-        setEditInterest(result.toString());
-    };
+  const handleChangeToggle = (value) => {
+    const result = [];
+    value.map((item) => result.push(item.value));
+    setEditInterest(result.toString());
+  };
 
-    const handleSubmitUpdateProfile = async (e) => {
-        try {
-            e.preventDefault();
-            await axios.put(`/about/${userData.id}`, {
-                age: age,
-                birthDate: date,
-                caption: profileCaption,
-                interest: editInterest,
-            });
-            const res = await axios.get(`/user/${username}`);
-            setPerson(res.data.user);
-        } catch (err) {
-            console.log(err.message);
-        }
-    };
-    return (
-        <div
-            className="modal fade "
-            id="EditProfileModal"
-            tabIndex="-1"
-            aria-labelledby="EditProfileModalLabel"
-            aria-hidden="true"
+  const handleSubmitUpdateProfile = async (e) => {
+    try {
+      e.preventDefault();
+      await axios.put(`/about/${userData.id}`, {
+        age: age,
+        birthDate: date,
+        caption: profileCaption,
+        interest: editInterest,
+      });
+      const res = await axios.get(`/user/${username}`);
+      setPerson(res.data.user);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+  return (
+    <div
+      className="modal fade "
+      id="EditProfileModal"
+      tabIndex="-1"
+      aria-labelledby="EditProfileModalLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog ">
+        <form
+          className="modal-content editProfileForm model-edit"
+          onSubmit={handleSubmitUpdateProfile}
         >
-            <div className="modal-dialog modal-fullscreen">
-                <form
-                    className="modal-content editProfileForm model-edit"
-                    onSubmit={handleSubmitUpdateProfile}
-                >
-                    <div className="modal-header mb-3 ">
-                        <h5 className="modal-title " id="EditProfileModalLabel">
-                            Edit Profile
-                        </h5>
-                        <button
-                            type="button"
-                            className="btn-close"
-                            data-bs-dismiss="modal"
-                            aria-label="Close"
-                        ></button>
-                    </div>
-                    <div className="modal-body mb-3">
-                        <div className="mb-3">
-                            <label htmlFor="dateofbirth" className="form-label">
-                                Date of Birth
-                            </label>
-                            <input
-                                type="date"
-                                className="form-control date-input"
-                                id="dateofbirth"
-                                value={date}
-                                onChange={(e) => setDate(e.target.value)}
-                            />
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="age" className="form-label">
-                                Age
-                            </label>
-                            <input
-                                type="text"
-                                className="form-control age-input"
-                                id="age"
-                                value={age}
-                                onChange={(e) => setAge(e.target.value)}
-                            />
-                        </div>
-                        <div className="mb-2">INTEREST</div>
-                        <Select
-                            isMulti
-                            name="interest"
-                            options={interestOption}
-                            className="basic-multi-select mb-3 selecter"
-                            classNamePrefix="select"
-                            onChange={handleChangeToggle}
-                        />
+          <div className="modal-header mb-3 ">
+            <h5 className="modal-title " id="EditProfileModalLabel">
+              Edit Profile
+            </h5>
+          </div>
+          <div className="modal-body mb-3">
+            <div className="mb-3">
+              <label htmlFor="dateofbirth" className="form-label">
+                Date of Birth
+              </label>
+              <input
+                type="date"
+                className="form-control date-input"
+                id="dateofbirth"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="age" className="form-label">
+                Age
+              </label>
+              <input
+                type="text"
+                className="form-control age-input"
+                id="age"
+                value={age}
+                onChange={(e) => setAge(e.target.value)}
+              />
+            </div>
+            <div className="mb-2">INTEREST</div>
+            <Select
+              isMulti
+              name="interest"
+              options={interestOption}
+              className="basic-multi-select mb-3 selecter"
+              classNamePrefix="select"
+              onChange={handleChangeToggle}
+            />
 
-                        <div className="mb-3">
-                            <label htmlFor="caption" className="form-label ">
-                                Caption
-                            </label>
-                            <input
-                                type="text"
-                                className="form-control caption-input"
-                                id="caption"
-                                value={profileCaption}
-                                onChange={(e) =>
-                                    setProfileCaption(e.target.value)
-                                }
-                            />
-                        </div>
-                    </div>
-                    <div className="modal-footer ">
-                        {/* <button
+            <div className="mb-3">
+              <label htmlFor="caption" className="form-label ">
+                Caption
+              </label>
+              <input
+                type="text"
+                className="form-control caption-input"
+                id="caption"
+                value={profileCaption}
+                onChange={(e) => setProfileCaption(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="modal-footer ">
+            <button
               type="button"
               className="btn closebutton"
               data-bs-dismiss="modal"
             >
               Close
-            </button> */}
-                        <button className="btn saveBtn" data-bs-dismiss="modal">
-                            Save changes
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
+            </button>
+            <button className="btn saveBtn" data-bs-dismiss="modal">
+              Save changes
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default EditProfileForm;
--- src/components/post/postform/PostForm.jsx
+++ src/components/post/postform/PostForm.jsx
@@ -1,163 +1,147 @@
-import axios from '../../../config/axios';
-import React, { useContext, useRef } from 'react';
-import Skeleton from '@mui/material/Skeleton';
-import { AuthContext } from '../../../contexts/AuthContext';
-import { PostContext } from '../../../contexts/PostContext';
-import './postform.css';
+import axios from "../../../config/axios";
+import React, { useContext, useRef } from "react";
+import Skeleton from "@mui/material/Skeleton";
+import { AuthContext } from "../../../contexts/AuthContext";
+import { PostContext } from "../../../contexts/PostContext";
+import "./postform.css";
 
 function PostForm() {
-    const { addPost, title, setTitle, picture, setPicture } =
-        useContext(PostContext);
-    const { loading, setLoading } = useContext(AuthContext);
-    const inputEl = useRef();
+  const { addPost, title, setTitle, picture, setPicture } =
+    useContext(PostContext);
+  const { loading, setLoading } = useContext(AuthContext);
+  const inputEl = useRef();
 
-    const handleSubmitPost = (e) => {
-        e.preventDefault();
-        addPost({ title, picture });
-        setTitle('');
-        setPicture('');
+  const handleSubmitPost = (e) => {
+    e.preventDefault();
+    addPost({ title, picture });
+    setTitle("");
+    setPicture("");
+  };
+
+  const handleFileInputChange = async (e) => {
+    setLoading(true);
+    if (!e.target.value) return;
+
+    const reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
+
+    reader.onloadend = async () => {
+      await uploadImage(reader.result);
     };
-
-    const handleFileInputChange = async (e) => {
-        setLoading(true);
-        if (!e.target.value) return;
-
-        const reader = new FileReader();
-        reader.readAsDataURL(e.target.files[0]);
-
-        reader.onloadend = async () => {
-            await uploadImage(reader.result);
-        };
-        e.target.value = '';
-        reader.onerror = () => {
-            console.error('AHHHHHHHH!!');
-        };
+    e.target.value = "";
+    reader.onerror = () => {
+      console.error("AHHHHHHHH!!");
     };
+  };
 
-    const uploadImage = async (base64EncodedImage) => {
-        try {
-            const res = await axios.post('/upload', {
-                data: base64EncodedImage,
-            });
-            setPicture(res.data.url);
-            setLoading(false);
-        } catch (err) {
-            alert('File size too large.');
-        }
-    };
+  const uploadImage = async (base64EncodedImage) => {
+    try {
+      const res = await axios.post("/upload", {
+        data: base64EncodedImage,
+      });
+      setPicture(res.data.url);
+      setLoading(false);
+    } catch (err) {
+      alert("File size too large.");
+    }
+  };
 
-    return (
-        <div
-            className="modal fade"
-            id="PostModal"
-            tabIndex="-1"
-            aria-labelledby="PostModalLabel"
-            aria-hidden="true"
-        >
-            <div className="modal-dialog ">
-                <div className="modal-content postForm">
-                    <form onSubmit={handleSubmitPost}>
-                        <div className="modal-header ">
-                            <h5
-                                className="modal-title postTitle"
-                                id="PostModalLabel"
-                            >
-                                Create Post
-                            </h5>
-                        </div>
-                        <div className="modal-body">
-                            {loading ? (
-                                <Skeleton
-                                    className="previewpostphoto"
-                                    variant="rectangular"
-                                    width={340}
-                                    height={263}
-                                />
-                            ) : picture && !loading ? (
-                                <div className="previewpostphoto d-flex">
-                                    <img
-                                        src={picture}
-                                        alt=""
-                                        className="previewpostphoto"
-                                    />
-                                    <button
-                                        type="button"
-                                        className="btn deletePhotoPreview"
-                                        onClick={() => setPicture('')}
-                                    >
-                                        <i className="bi bi-x-circle-fill"></i>
-                                    </button>
-                                </div>
-                            ) : (
-                                <>
-                                    <label
-                                        htmlFor="postFile"
-                                        className="form-label"
-                                    >
-                                        Select your photo :
-                                    </label>
-                                    <img
-                                        src="https://res.cloudinary.com/dbtlgaii3/image/upload/v1645373932/Placeholder_view_vector_yxqu8m.svg"
-                                        alt=""
-                                        className="previewpostphoto"
-                                        onClick={() => inputEl.current.click()}
-                                    />
-                                </>
-                            )}
-                            <div className="mb-2">
-                                <label
-                                    htmlFor="caption"
-                                    className="col-form-label postTitle"
-                                >
-                                    Caption :
-                                </label>
-                                <textarea
-                                    className="form-control"
-                                    id="caption"
-                                    value={title}
-                                    onChange={(e) => setTitle(e.target.value)}
-                                ></textarea>
-                            </div>
-                            <div className="mb-3">
-                                <input
-                                    className="form-control d-none"
-                                    type="file"
-                                    id="postFile"
-                                    ref={inputEl}
-                                    onChange={handleFileInputChange}
-                                />
-                            </div>
-                        </div>
-                        <div className="modal-footer">
-                            <button
-                                type="button"
-                                className="btn btn-secondary"
-                                data-bs-dismiss="modal"
-                            >
-                                Close
-                            </button>
-                            {picture && !loading ? (
-                                <button
-                                    className="btn postBtn"
-                                    data-bs-dismiss="modal"
-                                >
-                                    Submit Post
-                                </button>
-                            ) : (
-                                <button
-                                    className="btn postBtn"
-                                    data-bs-dismiss="modal"
-                                    disabled
-                                >
-                                    Submit Post
-                                </button>
-                            )}
-                        </div>
-                    </form>
-                </div>
+  return (
+    <div
+      className="modal fade"
+      id="PostModal"
+      tabIndex="-1"
+      aria-labelledby="PostModalLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog ">
+        <div className="modal-content postForm">
+          <form onSubmit={handleSubmitPost}>
+            <div className="modal-header ">
+              <h5 className="modal-title postTitle" id="PostModalLabel">
+                Create Post
+              </h5>
             </div>
+            <div className="modal-body">
+              {loading ? (
+                <Skeleton
+                  className="previewpostphoto"
+                  variant="rectangular"
+                  width={340}
+                  height={263}
+                />
+              ) : picture && !loading ? (
+                <div className="previewpostphoto d-flex">
+                  <img src={picture} alt="" className="previewpostphoto" />
+                  <button
+                    type="button"
+                    className="btn deletePhotoPreview"
+                    onClick={() => setPicture("")}
+                  >
+                    <i className="bi bi-x-circle-fill"></i>
+                  </button>
+                </div>
+              ) : (
+                <>
+                  <label htmlFor="postFile" className="form-label">
+                    Select your photo :
+                  </label>
+                  <img
+                    src="https://res.cloudinary.com/dbtlgaii3/image/upload/v1645373932/Placeholder_view_vector_yxqu8m.svg"
+                    alt=""
+                    className="previewpostphoto"
+                    onClick={() => inputEl.current.click()}
+                  />
+                </>
+              )}
+              <div className="mb-2">
+                <label htmlFor="caption" className="col-form-label postTitle">
+                  Caption :
+                </label>
+                <textarea
+                  className="form-control"
+                  id="caption"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                ></textarea>
+              </div>
+              <div className="mb-3">
+                <input
+                  className="form-control d-none"
+                  type="file"
+                  id="postFile"
+                  ref={inputEl}
+                  onChange={handleFileInputChange}
+                />
+              </div>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              {picture && !loading ? (
+                <button className="btn postBtnSubmit" data-bs-dismiss="modal">
+                  Submit Post
+                </button>
+              ) : (
+                <button
+                  className="btn postBtn"
+                  data-bs-dismiss="modal"
+                  disabled
+                >
+                  Submit Post
+                </button>
+              )}
+            </div>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default PostForm;
