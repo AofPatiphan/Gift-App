42612d727f2ee330a65f036ea919ee19a005437b
NewChakrit <imnew108@gmail.com>
2022-03-02 01:51:14 -0800

    'savenew'

--- src/config/env.jsx
+++ src/config/env.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 const API_ENDPOINT_URL = "http://localhost:8888";
-=======
-const API_ENDPOINT_URL = 'http://localhost:8888';
->>>>>>> 511e9f54cf26e0226c4efd81188e593e4bdb6d9c
 // const API_ENDPOINT_URL = 'https://soodpung-gift-app.herokuapp.com';
 
 export { API_ENDPOINT_URL };
